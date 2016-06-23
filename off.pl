fcc8b748729719323152eeee0abc15a1a48f7f50
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-23 09:46:22 -0700

    Added on() and off()

---
+++ off.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+# This script tells the daemon to turn things off.
+#
+use strict;
+do libPhiNet.pl;
+off($_);
---
+++ on.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+# This script tells the daemon to turn things on.
+#
+use strict;
+do libPhiNet.pl;
+on($_);
