c93aea94dd94b04dabad5177a1fb7c0db3d5adca
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-23 10:12:32 -0700

    Added an if to the example config file.
    I did this to:
    * Demonstrate the power of a config file w/ executable perl.
    * Have 1 config file.

--- phiNet.conf.pl
+++ phiNet.conf.pl
@@ -1,7 +1,21 @@
 # Example config file.
-#
+# FYI: This is executable perl code. All of its rules apply.
 use strict;
-# On my system/ the web root is /var/www/html.
-$daemonpidfile = "/var/www/phiNet/pid.conf.pl";
-$ipcfile       = "/var/www/phiNet/ipc.conf.pl";
-$
+
+$prod            = "0";
+# Whether in production or not.
+if($prod)
+{
+  # On my system/ the web root is /var/www/html.
+  $daemonpidfile = "/var/www/phiNet/pid.conf.pl";
+  # Where the file for the daemon's PID will be stored.
+  $ipcfile       = "/var/www/phiNet/ipc.conf.pl";
+  # Where the IPC file will be
+}
+else
+{
+  $daemonpidfile = "./pid.conf.pl";
+  # Where the file for the daemon's PID will be stored.
+  $ipcfile       = "./ipc.conf.pl";
+  # Where the IPC file will be
+}
