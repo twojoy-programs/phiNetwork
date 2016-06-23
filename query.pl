45b48d11536d295afca4926f4ef1aa9d152a72f0
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-23 11:47:32 -0700

    Modified to use new var

--- query.pl
+++ query.pl
@@ -7,6 +7,11 @@ use Digest::SHA qw(sha256_hex);
 use CGI
 my $config = "./phiNet.conf.pl";
 #my $config = "/var/www/phiNet.conf.pl"; #This is a better line if you use this in prod.
-do $config
-
+do $config;
+do $data;
+my $query = CGI->new;
 my @keywords = $query->keywords;
+if ($keywords{"test"} = "larrywall")
+{
+
+}
