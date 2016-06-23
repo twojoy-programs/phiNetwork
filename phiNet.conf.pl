6c31d6aec468dd600d2d7951f1d9d09c5293a51f
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-23 10:00:00 -0700

    Added example config file.

---
+++ phiNet.conf.pl
@@ -0,0 +1,7 @@
+# Example config file.
+#
+use strict;
+# On my system/ the web root is /var/www/html.
+$daemonpidfile = "/var/www/phiNet/pid.conf.pl";
+$ipcfile       = "/var/www/phiNet/ipc.conf.pl";
+$
