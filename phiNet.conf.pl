c4e9155af42a6c3cdb38e842c86ed749e03a120c
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-23 10:26:56 -0700

    Added explicit declarationto conf file.

--- phiNet.conf.pl
+++ phiNet.conf.pl
@@ -2,20 +2,22 @@
 # FYI: This is executable perl code. All of its rules apply.
 use strict;
 
-$prod            = "0";
+my $prod            = "0";
 # Whether in production or not.
 if($prod)
 {
   # On my system/ the web root is /var/www/html.
-  $daemonpidfile = "/var/www/phiNet/pid.conf.pl";
+  my $daemonpidfile = "/var/www/phiNet/pid.conf.pl";
   # Where the file for the daemon's PID will be stored.
-  $ipcfile       = "/var/www/phiNet/ipc.conf.pl";
+  my $ipcfile       = "/var/www/phiNet/ipc.conf.pl";
   # Where the IPC file will be
 }
 else
 {
-  $daemonpidfile = "./pid.conf.pl";
+  my $daemonpidfile = "./pid.conf.pl";
   # Where the file for the daemon's PID will be stored.
-  $ipcfile       = "./ipc.conf.pl";
+  my $ipcfile       = "./ipc.conf.pl";
   # Where the IPC file will be
 }
+my $password        = "";
+# This is a SHA1 hash of the actual PW.
