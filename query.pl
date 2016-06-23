e4bca584e7ce42660bcd190ba12638aa6d7a8b34
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-23 11:09:31 -0700

    Added CGI Backend (WIP)

---
+++ query.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+#
+#
+
+use strict;
+use Digest::SHA qw(sha256_hex);
+use CGI
+my $config = "./phiNet.conf.pl";
+#my $config = "/var/www/phiNet.conf.pl"; #This is a better line if you use this in prod.
+do $config
+
+my @keywords = $query->keywords;
