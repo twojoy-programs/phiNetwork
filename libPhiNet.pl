47e30ad567b6932a4145406ee51c4f656795cd3c
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-23 00:55:51 -0700

    Added on()

--- libPhiNet.pl
+++ libPhiNet.pl
@@ -1,11 +1,27 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -d
+
+my $config = "";
 # Library for talking w/ phinet daemon
 #
+use strict;
+do $config;
 sub on()
 {
-
+    my $arg = $_; # IDK why I did this, but I want to.
+    if (-e $file)
+    {
+      while(-e $file)
+      {
+        sleep(0.5);
+      }
+    }
+    open(PFILE, ">", $file);
+    print(PFILE, '$pid = ' . "$$\n");
+    print(PFILE, '$relay = ' . "$arg\n")
+    print(PFILE, '$state = ' . "$1\n")
 }
 sub off()
 {
 
 }
+1;
