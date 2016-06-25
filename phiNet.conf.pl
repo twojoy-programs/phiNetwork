066017cd9f32881f17cf58e05bca407cc16bb58f
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-25 09:49:20 -0700

    Added something to show how the config should be included.

--- phiNet.conf.pl
+++ phiNet.conf.pl
@@ -1,5 +1,9 @@
 # Example config file.
 # FYI: This is executable perl code. All of its rules apply.
+#
+# FYI: This is how it should be used.
+#my $config = "./phiNet.conf.pl";
+#my $config = "/var/www/phiNet.conf.pl"; # This is a better line if you use this in prod.
 
 use strict;
 my $prod            = "0";
