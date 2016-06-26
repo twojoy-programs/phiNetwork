f0c37c14ddf4cc6d59d9c8c5bf7026e5e1a762a6
Tucker R. Twomey <tt.runscript@gmail.com>
2016-06-25 19:40:28 -0700

    Fixed bugs.

--- gpiolib.test.pl
+++ gpiolib.test.pl
@@ -1,12 +1,13 @@
 # Test gpio library.
 #
-use strict;
+#use strict;
+use warnings;
 sub gpio_pulse()
 {
   my $arg = shift;
   my $fh;
   open($fh, ">>", "log.txt");
-  print($fh, "Pulsed pin #$arg");
+  print $fh "Pulsed pin #" . $arg . "\n";
   close($fh);
 }
 1;
