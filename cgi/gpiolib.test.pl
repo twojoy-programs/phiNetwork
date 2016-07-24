4d8864e06b83d99facb8c44eff37554b52ec1b6d
Tucker R. Twomey <tt.runscript@gmail.com>
2016-07-24 12:14:39 -0700

    Added export and unexport to gpio test library

--- cgi/gpiolib.test.pl
+++ cgi/gpiolib.test.pl
@@ -10,4 +10,20 @@ sub gpio_pulse()
   print $fh "Pulsed pin #" . $arg . "\n";
   close($fh);
 }
+sub gpio_unexport()
+{
+  my $arg = shift;
+  my $fh;
+  open($fh, ">>", "log.txt");
+  print $fh "Exported pin #" . $arg . "\n";
+  close($fh);
+}
+sub gpio_export()
+{
+  my $arg = shift;
+  my $fh;
+  open($fh, ">>", "log.txt");
+  print $fh "Unexported pin #" . $arg . "\n";
+  close($fh);
+}
 1;
