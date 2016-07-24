7e477f56732be859277183e06156504760d41ae8
Tucker R. Twomey <tt.runscript@gmail.com>
2016-07-24 14:39:19 -0700

    Changed how errors are declared

--- cgi/query.data.pl
+++ cgi/query.data.pl
@@ -1,27 +1,36 @@
-my $error_wrongform = "Content-Type:text/html\n\n"
-                      "<html><head><title>Query Error</title>\n"
-                      '<link rel="stylesheet" type="text/css" href="common.css">'."\n"
-                      '</head><body><h1 class="heading">Error:</h1><br>'."\n"
-                      '<strong class="center">Wrong form submitted</strong>'."\n"
-                      "</body></html>\n";
+my $error_wrongform = <<"END";
+"Content-Type:text/html\n\n
+<html><head><title>Query Error</title>\n
+<link rel="stylesheet" type="text/css" href="common.css">\n
+</head><body><h1 class="heading">Error:</h1><br>\n
+<strong class="center">Wrong form submitted</strong>\n
+</body></html>\n
+END
 
-my $error_wrongpw   = "Content-Type:text/html\n\n"
-                      "<html><head><title>Query Error</title>\n"
-                      '<link rel="stylesheet" type="text/css" href="common.css">'."\n"
-                      '</head><body><h1 class="heading">Error:</h1><br>'."\n"
-                      '<strong class="center">Password Incorrect</strong>'."\n"
-                      "</body></html>\n";
+my $error_wrongpw = <<"END";
+"Content-Type:text/html\n\n
+<html><head><title>Query Error</title>\n
+<link rel="stylesheet" type="text/css" href="common.css">\n
+</head><body><h1 class="heading">Error:</h1><br>\n
+<strong class="center">Incorrect Password!</strong>\n
+</body></html>\n
+END
 
-my $success         = "Content-Type:text/html\n\n"
-                      "<html><head><title>Success!</title>\n"
-                      '<link rel="stylesheet" type="text/css" href="common.css">'."\n"
-                      '</head><body><h1 class="heading">Success!!!</h1><br>'."\n"
-                      '<strong class="center"></strong>'."\n"
-                      "</body></html>\n";
+my $success = <<"END";
+"Content-Type:text/html\n\n
+<html><head><title>Query Success!</title>\n
+<link rel="stylesheet" type="text/css" href="common.css">\n
+</head><body><h1 class="heading">Query Successful!</h1><br>\n
+</body></html>\n
+END
 
-my $error_daemondead ="Content-Type:text/html; charset=UTF-8\n"
-                      "<html><head><title>Query Error</title>\n"
-                      '<link rel="stylesheet" type="text/css" href="common.css">'."\n"
-                      '</head><body><h1 class="heading">Error:</h1><br>'."\n"
-                      '<strong class="center">The daemon doesn\'t seem to be up</strong>'."\n"
-                      "</body></html>\n";
+my $error_daemondead = <<"END";
+"Content-Type:text/html\n\n
+<html><head><title>Query Error</title>\n
+<link rel="stylesheet" type="text/css" href="common.css">\n
+</head><body><h1 class="heading">Error:</h1><br>\n
+<strong class="center">phiNetd is either not running, or the lockfile is
+somewhere that this program can't find. Please show this error to whoever is
+responsible for fixing this problem.</strong>\n
+</body></html>\n
+END
